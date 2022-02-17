3cd81e1fc486b253f63d3ea9ea519f0195482b68
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-02-17 13:19:31 -0300

    CREATE (PROJECT): ORGANIZE PROJECT STRUCT
    DESCRIPTION:
    - Create project
    - Create folder to organize default files
    - Create first empty test class

---
+++ MemoryLeakUnitTest/MemoryLeakUnitTest/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MemoryLeakUnitTest
+//
+//  Created by Paolo Prodossimo Lopes on 17/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
---
+++ MemoryLeakUnitTest/MemoryLeakUnitTestTests/FirstArticleMemoryLeakTest.swift
@@ -0,0 +1,13 @@
+//
+//  FirstArticleMemoryLeakTest.swift
+//  MemoryLeakUnitTestTests
+//
+//  Created by Paolo Prodossimo Lopes on 17/02/22.
+//
+
+import Foundation
+import XCTest
+
+final class FirstArticleMememoryLeakTest: XCTestCase {
+    
+}
